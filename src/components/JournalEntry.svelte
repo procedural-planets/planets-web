9359b8ad428b8d1c37a1bdefa6d3a2de240c8525
albinkempe <albin.kempe@outlook.com>
2023-05-07 17:55:01 +0200

    created basic layout

---
+++ src/components/JournalEntry.svelte
@@ -0,0 +1,42 @@
+<script>
+    /**
+     * @type {string}
+     */
+     export let entry_title;
+     /**
+     * @type {string}
+     */
+    export let entry_text;
+    /**
+     * @type {string}
+     */
+     export let entry_date;
+</script>
+
+<div class="journal-entry">
+    <h2>{entry_title}</h2>
+    <p class="date">{entry_date}</p>
+    <p>{entry_text}</p>
+</div>
+
+<style lang="scss">
+    .journal-entry {
+        background-color: rgb(9, 41, 39);
+        width: 100%;
+        border-radius: 15px;
+        box-shadow: 0 4px 6px 0 #00000033;
+        padding: 1em 2em 2em 2em;
+        margin-bottom: 4em;
+    }
+
+    .date {
+        color: rgb(118, 153, 151);
+        font-weight: bold;
+        padding-top: 0;
+    }
+
+    p {
+        padding-top: 1em;
+        white-space: pre-line;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,91 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import '../reset.css'
+    import JournalEntry from '../components/JournalEntry.svelte';
+</script>
+
+<main>
+    <div class="title">
+        <h1>Procedural Planets</h1>
+        <p>by Albin Kempe, André Fredriksen & Vilhelm Hellmér</p>
+    </div>
+    <div class="jorunal">
+        <JournalEntry
+            entry_title = "Second Entry"
+            entry_date = "2023-05-07"
+            entry_text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mattis vulputate enim nulla aliquet porttitor lacus luctus. In nisl nisi scelerisque eu ultrices vitae auctor eu augue. Id aliquet risus feugiat in ante metus dictum at. Quisque id diam vel quam elementum pulvinar etiam non. Sed blandit libero volutpat sed cras ornare arcu. Aliquam eleifend mi in nulla posuere sollicitudin aliquam ultrices. Diam in arcu cursus euismod quis viverra nibh cras pulvinar. Cursus in hac habitasse platea dictumst. Vulputate dignissim suspendisse in est.
+
+            Egestas dui id ornare arcu odio ut sem. In cursus turpis massa tincidunt dui ut ornare lectus. Semper risus in hendrerit gravida. Erat pellentesque adipiscing commodo elit. Duis ut diam quam nulla porttitor. Ac feugiat sed lectus vestibulum mattis. Lacus vel facilisis volutpat est velit egestas dui id ornare. Gravida cum sociis natoque penatibus. Sit amet consectetur adipiscing elit duis tristique sollicitudin nibh. Neque vitae tempus quam pellentesque nec nam aliquam. At ultrices mi tempus imperdiet nulla. Integer enim neque volutpat ac tincidunt vitae semper. Pellentesque sit amet porttitor eget. Sed turpis tincidunt id aliquet risus feugiat in. Eu consequat ac felis donec. Pulvinar mattis nunc sed blandit libero volutpat sed cras.
+            
+            Pellentesque sit amet porttitor eget dolor morbi. Eu turpis egestas pretium aenean pharetra magna ac. Amet tellus cras adipiscing enim eu turpis egestas. Morbi tincidunt ornare massa eget. Vel quam elementum pulvinar etiam non quam lacus suspendisse faucibus. Ultrices neque ornare aenean euismod elementum nisi quis eleifend quam. Sapien et ligula ullamcorper malesuada proin libero nunc consequat interdum. Pulvinar sapien et ligula ullamcorper malesuada proin. Leo integer malesuada nunc vel risus commodo viverra maecenas. Donec et odio pellentesque diam volutpat commodo sed egestas egestas. Rhoncus mattis rhoncus urna neque viverra justo nec. Imperdiet nulla malesuada pellentesque elit. Id cursus metus aliquam eleifend. Ac tortor dignissim convallis aenean et.
+            
+            Nullam vehicula ipsum a arcu cursus vitae congue mauris. Posuere lorem ipsum dolor sit amet consectetur. Turpis egestas maecenas pharetra convallis posuere morbi leo. In cursus turpis massa tincidunt. Fermentum odio eu feugiat pretium nibh. Ornare suspendisse sed nisi lacus sed viverra tellus in hac. In ornare quam viverra orci sagittis eu volutpat odio facilisis. Libero id faucibus nisl tincidunt eget nullam non nisi est. Tincidunt tortor aliquam nulla facilisi cras fermentum. Nisi scelerisque eu ultrices vitae auctor. Enim nunc faucibus a pellentesque. Scelerisque felis imperdiet proin fermentum leo vel orci porta. Amet venenatis urna cursus eget nunc scelerisque. Mattis pellentesque id nibh tortor. Suscipit adipiscing bibendum est ultricies integer quis auctor. Adipiscing diam donec adipiscing tristique risus nec feugiat. Lorem mollis aliquam ut porttitor leo a diam sollicitudin.
+            
+            Amet massa vitae tortor condimentum lacinia quis vel. Sed euismod nisi porta lorem mollis aliquam ut. Semper risus in hendrerit gravida rutrum quisque non tellus. Fames ac turpis egestas maecenas pharetra convallis posuere morbi leo. Id velit ut tortor pretium. Ullamcorper morbi tincidunt ornare massa. Et netus et malesuada fames ac turpis. Etiam non quam lacus suspendisse faucibus interdum. Metus dictum at tempor commodo ullamcorper a lacus vestibulum. Risus pretium quam vulputate dignissim suspendisse in est ante in. Cursus metus aliquam eleifend mi in nulla posuere sollicitudin aliquam. Urna nunc id cursus metus aliquam eleifend."
+        />
+        <JournalEntry
+            entry_title = "First Entry"
+            entry_date = "2023-05-06"
+            entry_text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Neque viverra justo nec ultrices dui sapien eget mi proin. Diam vel quam elementum pulvinar etiam non quam lacus. Proin fermentum leo vel orci porta non pulvinar. Enim diam vulputate ut pharetra sit amet. Nisi quis eleifend quam adipiscing vitae proin sagittis. Cras tincidunt lobortis feugiat vivamus at augue eget arcu. Nam libero justo laoreet sit amet cursus sit amet. Pellentesque nec nam aliquam sem et tortor consequat id porta. Dictumst quisque sagittis purus sit amet volutpat. Vitae auctor eu augue ut lectus arcu bibendum at. A pellentesque sit amet porttitor eget dolor. Ut etiam sit amet nisl.
+
+            In eu mi bibendum neque. Dui accumsan sit amet nulla facilisi morbi. At volutpat diam ut venenatis tellus in. Nibh sit amet commodo nulla. Cursus euismod quis viverra nibh. Et ligula ullamcorper malesuada proin libero nunc consequat interdum. Massa tincidunt nunc pulvinar sapien. Ornare quam viverra orci sagittis eu. In nibh mauris cursus mattis molestie. Placerat orci nulla pellentesque dignissim enim sit.
+            
+            Scelerisque mauris pellentesque pulvinar pellentesque habitant morbi tristique senectus et. Adipiscing elit pellentesque habitant morbi tristique senectus et netus et. Quis ipsum suspendisse ultrices gravida dictum fusce. Est lorem ipsum dolor sit amet consectetur adipiscing elit pellentesque. Id nibh tortor id aliquet lectus proin nibh nisl. Commodo viverra maecenas accumsan lacus vel facilisis. Lectus sit amet est placerat in egestas. Sed odio morbi quis commodo odio. Enim praesent elementum facilisis leo vel fringilla est. Arcu cursus vitae congue mauris. Non pulvinar neque laoreet suspendisse interdum consectetur libero id. Amet consectetur adipiscing elit duis tristique sollicitudin nibh. Tellus molestie nunc non blandit massa. Scelerisque mauris pellentesque pulvinar pellentesque habitant.
+            
+            Massa vitae tortor condimentum lacinia quis vel eros. Quis ipsum suspendisse ultrices gravida dictum fusce ut placerat orci. Ultricies integer quis auctor elit sed vulputate mi. Arcu felis bibendum ut tristique et egestas quis ipsum suspendisse. Nisl pretium fusce id velit ut tortor pretium viverra. Commodo odio aenean sed adipiscing diam donec adipiscing tristique risus. Eget est lorem ipsum dolor sit. At ultrices mi tempus imperdiet. Sem fringilla ut morbi tincidunt augue interdum velit euismod. Elit eget gravida cum sociis. Enim lobortis scelerisque fermentum dui faucibus. Hac habitasse platea dictumst quisque sagittis purus. Lorem mollis aliquam ut porttitor. Elementum facilisis leo vel fringilla est ullamcorper eget nulla. Adipiscing at in tellus integer feugiat. Lorem ipsum dolor sit amet.
+            
+            Lorem ipsum dolor sit amet consectetur adipiscing elit duis. In nibh mauris cursus mattis. Vitae tempus quam pellentesque nec nam aliquam sem. Ullamcorper a lacus vestibulum sed. Accumsan tortor posuere ac ut consequat semper viverra. Odio tempor orci dapibus ultrices in. Natoque penatibus et magnis dis parturient. Lacus laoreet non curabitur gravida arcu ac tortor dignissim convallis. Velit aliquet sagittis id consectetur purus ut. Id volutpat lacus laoreet non curabitur gravida. Dolor magna eget est lorem ipsum dolor sit. Nullam ac tortor vitae purus faucibus ornare suspendisse sed."
+        />
+    </div>
+    <footer>
+        <p>KTH DH2323</p>
+    </footer>
+</main>
+
+<style lang="scss">
+	@import url('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,700;1,400&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;1,400&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400;0,700;1,400&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=Righteous&display=swap');
+
+    :global(:root) {
+        font-family: 'Lato';
+        font-size: 1.5vh;
+        background-color: rgb(15, 65, 62);
+        color: white;
+    }
+
+    :global(h1) {
+        font-family: 'EB Garamond';
+        font-size: 3vh;
+        text-align: center;
+    }
+
+    :global(h2) {
+        font-family: 'EB Garamond';
+        font-size: 3vh;
+    }
+
+    main {
+        width: 50vw;
+        height: 100vh;
+        margin: auto;
+    }
+
+    .title {
+        padding-top: 2em;
+        padding-bottom: 2em;
+    }
+
+    .title p {
+        text-align: center;
+    }
+
+    footer p {
+        text-align: center;
+        color:rgb(124, 158, 155)
+    }
+
+    footer {
+        height: 4em;
+    }
+</style>
